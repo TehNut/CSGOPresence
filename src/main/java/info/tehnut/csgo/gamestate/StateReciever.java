a6cf0f0234d029042b9bd87b4752585d21f133e5
Nicholas Ignoffo <nick@tehnut.info>
2017-12-19 19:41:15 -0800

    Add time printout to state message

--- src/main/java/info/tehnut/csgo/gamestate/StateReciever.java
+++ src/main/java/info/tehnut/csgo/gamestate/StateReciever.java
@@ -8,17 +8,21 @@ import info.tehnut.csgo.gamestate.data.GameState;
 import info.tehnut.csgo.util.IOUtils;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class StateReciever implements HttpHandler {
 
     private static final Gson GSON = new GsonBuilder().serializeNulls().setPrettyPrinting().disableHtmlEscaping().create();
+    private static final DateFormat FORMAT = new SimpleDateFormat("HH:mm:ss");
     private static GameState oldState;
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         String response = IOUtils.toString(httpExchange.getRequestBody());
         GameState gameState = GSON.fromJson(response, GameState.class);
-        System.out.println("Received state update from " + gameState.getProvider().getName() + ". Posting to watchers.");
+        System.out.println(FORMAT.format(new Date()) + " | Received state update from " + gameState.getProvider().getName() + ". Posting to watchers.");
 
         for (IStateUpdateWatcher watcher : CSGOGamestate.UPDATE_WATCHERS)
             watcher.handleUpdatedState(gameState, oldState);
