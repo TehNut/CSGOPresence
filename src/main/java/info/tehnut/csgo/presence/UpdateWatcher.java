1439cffe7e2e48318f907182d0e368781bd1b33e
Nicholas Ignoffo <nick@tehnut.info>
2018-03-24 16:09:05 -0700

    Spectation status(maybe)

--- src/main/java/info/tehnut/csgo/presence/UpdateWatcher.java
+++ src/main/java/info/tehnut/csgo/presence/UpdateWatcher.java
@@ -5,48 +5,50 @@ import info.tehnut.csgo.gamestate.IStateUpdateWatcher;
 import info.tehnut.csgo.gamestate.data.GameMap;
 import info.tehnut.csgo.gamestate.data.GameState;
 import info.tehnut.csgo.gamestate.data.MatchStats;
-import info.tehnut.csgo.gamestate.data.Weapon;
 
 import java.util.Locale;
 
 public class UpdateWatcher implements IStateUpdateWatcher {
 
     public void handleUpdatedState(GameState newState) {
-        if (newState.isOurUser()) {
-            if (newState.getMap() != null) { // We're in a match of some kind
-                GameMap map = newState.getMap();
+        if (newState.getMap() != null) { // We're in a match of some kind
+            GameMap map = newState.getMap();
+            GameModes gameMode = GameModes.getbyName(map.getMode());
+            CSGOPresence.DISCORD_PRESENCE.details = capitalize(gameMode.getName()); // deathmatch -> Deathmatch
 
-                MapImages mapImage = MapImages.getbyName(map.getName());
-                CSGOPresence.DISCORD_PRESENCE.largeImageKey = mapImage.getImage();
-                CSGOPresence.DISCORD_PRESENCE.largeImageText = "Map: " + map.getName();
+            if (!newState.isOurUser())
+                CSGOPresence.DISCORD_PRESENCE.details += " (Spectating)";
 
-                GameModes gameMode = GameModes.getbyName(map.getMode());
-                CSGOPresence.DISCORD_PRESENCE.details = capitalize(gameMode.getName()); // deathmatch -> Deathmatch
+            MapImages mapImage = MapImages.getbyName(map.getName());
+            CSGOPresence.DISCORD_PRESENCE.largeImageKey = mapImage.getImage();
+            CSGOPresence.DISCORD_PRESENCE.largeImageText = "Map: " + map.getName();
 
-                String scoreText = "Score: ";
-                if (gameMode.isTeamOriented()) { // If we're in a team-based mode (ie: Defusal)
-                    boolean ct = newState.getPlayer().getTeam().equalsIgnoreCase("ct");
-                    if (ct)
-                        scoreText += map.getCounterTerrorist().getScore() + " - " + map.getTerrorist().getScore();
-                    else
-                        scoreText += map.getTerrorist().getScore() + " - " + map.getCounterTerrorist().getScore();
-                } else { // Non-team-based (ie: Death match)
-                    scoreText += newState.getPlayer().getMatchStats().getScore();
-                }
-
-                CSGOPresence.DISCORD_PRESENCE.smallImageKey = "sdefault";
-                MatchStats matchStats = newState.getPlayer().getMatchStats();
-                CSGOPresence.DISCORD_PRESENCE.smallImageText = String.format("%dK / %dA / %dD", matchStats.getKills(), matchStats.getAssists(), matchStats.getDeaths());
-
-                CSGOPresence.DISCORD_PRESENCE.state = scoreText;
-            } else { // We're not in a match. Must be in the main menu... or at least a menu of some sort... probably
-                CSGOPresence.DISCORD_PRESENCE.details = "In menu";
-                CSGOPresence.DISCORD_PRESENCE.state = null;
-                CSGOPresence.DISCORD_PRESENCE.largeImageKey = MapImages.UNKNOWN.getImage();
-                CSGOPresence.DISCORD_PRESENCE.largeImageText = null;
-                CSGOPresence.DISCORD_PRESENCE.smallImageKey = null;
-                CSGOPresence.DISCORD_PRESENCE.smallImageText = null;
+            String scoreText = "Score: ";
+            if (gameMode.isTeamOriented()) { // If we're in a team-based mode (ie: Defusal)
+                boolean ct = newState.getPlayer().getTeam().equalsIgnoreCase("ct");
+                if (ct)
+                    scoreText += map.getCounterTerrorist().getScore() + " - " + map.getTerrorist().getScore();
+                else
+                    scoreText += map.getTerrorist().getScore() + " - " + map.getCounterTerrorist().getScore();
+            } else if (newState.isOurUser()) { // Non-team-based (ie: Death match)
+                scoreText += newState.getPlayer().getMatchStats().getScore();
             }
+
+            if (!newState.isOurUser())
+                return;
+
+            CSGOPresence.DISCORD_PRESENCE.smallImageKey = "sdefault";
+            MatchStats matchStats = newState.getPlayer().getMatchStats();
+            CSGOPresence.DISCORD_PRESENCE.smallImageText = String.format("%dK / %dA / %dD", matchStats.getKills(), matchStats.getAssists(), matchStats.getDeaths());
+
+            CSGOPresence.DISCORD_PRESENCE.state = scoreText;
+        } else { // We're not in a match. Must be in the main menu... or at least a menu of some sort... probably
+            CSGOPresence.DISCORD_PRESENCE.details = "In menu";
+            CSGOPresence.DISCORD_PRESENCE.state = null;
+            CSGOPresence.DISCORD_PRESENCE.largeImageKey = MapImages.UNKNOWN.getImage();
+            CSGOPresence.DISCORD_PRESENCE.largeImageText = null;
+            CSGOPresence.DISCORD_PRESENCE.smallImageKey = null;
+            CSGOPresence.DISCORD_PRESENCE.smallImageText = null;
         }
 
         DiscordRPC.INSTANCE.Discord_UpdatePresence(CSGOPresence.DISCORD_PRESENCE);
